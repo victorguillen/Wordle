508a958f107ee7dbb992a184d080ec9fdd667853
Victor Guillen <victor.guillen.ch@gmail.com>
2022-11-05 20:20:32 -0700

    fixed conflict

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -2,14 +2,9 @@ import React, { useState, useEffect } from 'react'
 import useGameLogic from '../hooks/useGameLogic'
 import Board from './Board'
 import Keypad from './Keypad'
-<<<<<<< Updated upstream
 import Modal from './Modals/Modal'
 import Toolbar from './Toolbar'
 import EndGameContent from './Modals/EndGameContent'
-=======
-import Modal from './Modal'
-import Title from './Title'
->>>>>>> Stashed changes
 
 interface Props {
   solution: string
@@ -34,11 +29,7 @@ const Wordle: React.FC<Props> = ({ solution }) => {
 
   return (
     <>
-<<<<<<< Updated upstream
       <Toolbar/>
-=======
-      <Title title="Wordle" />
->>>>>>> Stashed changes
       <Board guesses={guesses} currentGuess={currentGuess} turn={turn} />
       <Keypad usedKeys={usedKeys} handleOnChange={handleOnChange} />
       <Modal open={showModal} onClose={() => setShowModal(false)}>
